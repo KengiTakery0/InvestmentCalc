49b0eb079c3d1bf62c048f19b5ee7ad9bf7e2371
Pro100ivan1205 <lotoshnicovivan2005@gmail.com>
2025-04-30 00:51:24 +0300

    Сделал импорт  EntityFrameWork, добавил AppdbContext

---
+++ Services/UserServices/Model/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace InvestmentCalc.Services.UserServices.Model
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("YourConnectionStringHere");
+        }
+    }
+}
--- Services/UserServices/Model/User.cs
+++ Services/UserServices/Model/User.cs
@@ -1,12 +1,18 @@
-﻿namespace InvestmentCalc.Services.UserServices
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace InvestmentCalc.Services.UserServices.Model
 {
-    public class UserModel
+    public class User
     {
+        [Key]
         public int UserID { get; set; }
+
         public required string UserName { get; set; }
         public required string Password { get; set; }
         public required string Email { get; set; }
 
         // TO DO DataToSave
     }
+
 }
