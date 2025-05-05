10d91a8b44f074ec3c818a38ea9380eb854f0771
Roman <74704965+KengiTakery0@users.noreply.github.com>
2025-05-05 05:24:05 +0300

    Update EmailService.cs

--- AuthService/Services/EmailService.cs
+++ AuthService/Services/EmailService.cs
@@ -1,9 +1,15 @@
-﻿namespace AuthService.Services
+﻿
+namespace AuthService.Services
 {
-    public class EmailService:IEmailService
+    public class EmailService: IEmailService
     {
         private readonly IConfiguration _config;
 
         public EmailService(IConfiguration config) => _config = config;
+
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
